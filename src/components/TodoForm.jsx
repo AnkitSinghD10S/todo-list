54c3c59a4418051f84fa975e9c3805cc38218673
Ankit Singh1 <Ankit.Singh1@Geminisolutions.com>
2025-04-07 09:36:28 +0530

    compl

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import { useTodo } from '../contexts';
+import { useTodo } from '../contexts/TodoContext';
 function TodoForm() {
     const[todo,setTodo] =useState("")
     const {addTodo} = useTodo()
